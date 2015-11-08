5bd04863a80c5c6ad13646fd5122426885b79872
Quint Guvernator <quintus.public@gmail.com>
2015-11-08 19:08:39 +0100

    better docs for ben

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # documnt
 backend for the documnt concrete poetry site
+
+## How do I run this thing?
+
+  - install python3
+  - install flask: `pip3 install flask`
+  - run the webapp: `sudo python3 webapp.py`
+  - visit [localhost](http://0.0.0.0)
+
+## Okay, what's there to see so far?
+
+There's art! You can see the same filler text in three different styles!
+
+  - [shitty prose](http://0.0.0.0/0/prose)
+  - [shitty traditional poetry](http://0.0.0.0/0/poetry)
+  - [shitty concrete poetry](http://0.0.0.0/0/mono)
+
+If you visit the [main URL for the item](http://0.0.0.0/0) it'll go to the preferred form without changing the URL (this is good). I set my shitty filler text to prefer to be seen as prose.
+
+## How do I get changes into the codebase/production?
+
+Read DEVELOP.md
+
+## Theoretical CMS overview
+
+Each artistic work gets a numeric ID. An artist can specify how s/he prefers the art to be viewed. Current viewing styles are:
+
+  - poetry: centered lines of text one after the other
+  - prose: a single column of justified text
+  - mono(space): raw text, preserving all spaces, with an 80-character limit
+
+For testing, there's this style-override you can do. The url is made up of three parts:
+
+    documnt.url/id/style
+
+…where `document.url` is a running instance of documnt, `id` is a positive integer, and `style` is one of the styles listed above. If style is not specified, the preferred style set for that work is used.  **Of course, explicit style selection won't be possible once the site is actually up.** It should just choose the right one and display it. This is just for testing.
+
+We can use whatever backend you want for storing and submitting each text-art-thing. Redis would be a good choice.
--- webapp.py
+++ webapp.py
@@ -3,15 +3,25 @@
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
-# DEBUG
+# just for testing
 from random import randrange
-def lorem():
+def lorem(): #DEBUG
     ipsum = lambda: " ".join(["Pack my bag with five dozen liquor jugs."] * randrange(5, 16))
     return "\n\n".join([ ipsum() for _ in range(randrange(5, 16)) ])
 
+def get_art(art_id):
+    if art_id != 0: raise FileNotFoundError("No such art!")
+    art = {}
+    art["text_type"] = "prose"
+    art["title"] = "What is art"
+    art["text"] = lorem()
+    art["author"] = "Nobody at all"
+    return art
+
 @app.route('/')
 def home():
-    return redirect(url_for('show_prose', art_id=28))
+    # just for testing
+    return redirect(url_for('show_prose', art_id=0)) #DEBUG
 
 @app.route('/authors')
 def authors():
@@ -29,21 +39,38 @@ def contact():
 def about_art(art_id):
     return "This would show an overview of art #{}.".format(art_id)
 
+def render_art(art):
+    if art["text_type"] == "mono":
+        template = "monospaced.html"
+    else:
+        template = "literary.html"
+
+    try:
+        return render_template(template, **art)
+    except FileNotFoundError:
+        return "No art found with id {}.".format(art_id)
+
 @app.route('/<int:art_id>')
 def show_art(art_id):
-    return "This would show art #{} in the artist-chosen format/s.".format(art_id)
+    return render_art(get_art(art_id))
 
 @app.route('/<int:art_id>/poetry')
 def show_poetry(art_id):
-    return render_template('literary.html', text_type="poetry", title=art_id, author="Nobody", text=lorem())
+    art = get_art(art_id)
+    art["text_type"] = "poetry"
+    return render_art(art)
 
 @app.route('/<int:art_id>/prose')
 def show_prose(art_id):
-    return render_template('literary.html', text_type="prose", title=art_id, author="Nobody", text=lorem())
+    art = get_art(art_id)
+    art["text_type"] = "prose"
+    return render_art(art)
 
 @app.route('/<int:art_id>/mono')
 def show_mono(art_id):
-    return render_template('monospaced.html', title=art_id, author="Nobody", text=lorem())
+    art = get_art(art_id)
+    art["text_type"] = "mono"
+    return render_art(art)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
