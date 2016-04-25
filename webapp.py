01a439ef04d255102b4640bbea7efe3145c17da0
mattarnold44 <matt.arnold44@gmail.com>
2016-04-25 16:15:24 -0400

    add index.html to webapp

--- webapp.py
+++ webapp.py
@@ -64,6 +64,10 @@ def home():
     return redirect(url_for('about')) #beta
     #return render_template("index.html")
 
+@app.route('/index')
+def index():
+    return render_template("index.html")
+
 @app.route('/about')
 def about():
     return render_template("about.html")  
