0f12777fb9ea9a5f83665c051fa16d766ce946bf
mattarnold44 <matt.arnold44@gmail.com>
2017-05-16 12:02:36 -0400

    change /archive to /files

--- webapp.py
+++ webapp.py
@@ -9,61 +9,61 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route('/index')
-def index():
-    return render_template("index.html")
+@app.route('/issues')
+def issues():
+    return render_template("issues.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")  
 
-@app.route('/subscribe')
-def subscribe():
-    return render_template("subscribe.html")
-
-@app.route('/issues')
-def issues():
-    return render_template("issues.html") 
-
 @app.route('/mixtapes')
 def mixtapes():
-    return render_template("mixtapes.html") 
+    return render_template("mixtapes.html")  
 
-@app.route('/art')
-def art():
-    return render_template("art.html") 
+@app.route('/files')
+def files():
+    return render_template("files.html") 
 
-@app.route('/archive')
-def archive():
-    return render_template("art.html") 
+# @app.route('/subscribe')
+# def subscribe():
+#     return render_template("subscribe.html")
 
-@app.route('/texts')
-def texts():
-    return render_template("texts.html")
+# @app.route('/art')
+# def art():
+#     return render_template("art.html") 
 
-@app.route('/videos')
-def videos():
-	return render_template("videos.html")
+# @app.route('/index')
+# def index():
+#     return render_template("index.html")
 
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")  
+# @app.route('/texts')
+# def texts():
+#     return render_template("texts.html")
 
-@app.route('/demo')
-def show_demo():
-    return render_template("demo_content.html")
+# @app.route('/videos')
+# def videos():
+# 	return render_template("videos.html")
 
-@app.route('/demo2')
-def show_demo2():
-    return render_template("demo_content2.html")
+# @app.route('/contact')
+# def contact():
+#     return render_template("contact.html")  
 
-@app.route('/demo3')
-def show_demo3():
-    return render_template("demo_content3.html")
+# @app.route('/demo')
+# def show_demo():
+#     return render_template("demo_content.html")
 
-@app.route('/demo4')
-def show_demo4():
-    return render_template("demo_content4.html")
+# @app.route('/demo2')
+# def show_demo2():
+#     return render_template("demo_content2.html")
+
+# @app.route('/demo3')
+# def show_demo3():
+#     return render_template("demo_content3.html")
+
+# @app.route('/demo4')
+# def show_demo4():
+#     return render_template("demo_content4.html")
 
 
 if __name__ == '__main__':
